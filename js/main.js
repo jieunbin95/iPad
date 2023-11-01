77731529fe138105f77ae9cee5719b18911c2f0a
jieunbin95 <bje0416@naver.com>
2023-11-01 14:13:43 +0900

    ipad-mobile

--- js/main.js
+++ js/main.js
@@ -47,7 +47,8 @@ const searchDelayEls = [...searchWrapEl.querySelectorAll('li')]
 // js콜백 파트에서 배울내용
 searchStarterEl.addEventListener('click', showSearch)
 
-searchCloserEl.addEventListener('click', function () {
+searchCloserEl.addEventListener('click', function (event) {
+  event.stopPropagation()
   hideSearch()
 })
 searchShadowEl.addEventListener('click', function () {
@@ -57,7 +58,7 @@ searchShadowEl.addEventListener('click', function () {
 function showSearch() {
   headerEl.classList.add('searching')
   // documentElement는 html을 말한다
-  document.documentElement.classList.add('fixed')
+  stopScroll()
   headerMenuEls.reverse().forEach(function (el, index) {
     el.style.transitionDelay = index * .4 / headerMenuEls.length + 's'
   })
@@ -73,7 +74,7 @@ function showSearch() {
 
 function hideSearch() {
   headerEl.classList.remove('searching')
-  document.documentElement.classList.remove('fixed')
+  playScroll()
   headerMenuEls.reverse().forEach(function (el, index) {
     el.style.transitionDelay = index * .4 / headerMenuEls.length + 's'
   })
@@ -84,6 +85,75 @@ function hideSearch() {
   // 검색창을 다시 켰을때 검색창에 아무것도 남아있지 않도록 설정
   searchInputEl.value = ''
 }
+
+function playScroll(){
+  document.documentElement.classList.remove('fixed')
+}
+
+function stopScroll(){
+  document.documentElement.classList.add('fixed')
+}
+
+//모바일 버전)헤더 메뉴 토글
+const menuStarterEl= document.querySelector('header .menu-starter')
+menuStarterEl.addEventListener('click',function(){
+  if(headerEl.classList.contains('menuing')){
+    headerEl.classList.remove('menuing')
+    searchInputEl.value = ''
+    playScroll()
+  } else{
+    headerEl.classList.add('menuing')
+    stopScroll()
+  }
+})
+
+// 헤더검색
+const searchTextFieldEl= document.querySelector('header .textfield')
+const searchCancelEl= document.querySelector('header .search-canceler')
+searchTextFieldEl.addEventListener('click',function(){
+  headerEl.classList.add('search--mobile')
+  // 검색창 클릭시 바로 입력할 수 있도록 focus()메서드를 실행시킨다
+  searchInputEl.focus()
+})
+searchCancelEl.addEventListener('click',function(){
+  headerEl.classList.remove('search--mobile')
+})  
+
+//화면의 사이즈가 바뀔때 마다 (resize)함수를 실행시킨다
+window.addEventListener('resize', function() {
+  if(window.innerWidth<=740){
+    headerEl.classList.remove('searching')
+  }else{
+    headerEl.classList.remove('search--mobile')
+  }
+})
+
+//네비바의 toggle버튼을 눌렀을 때 menu보이게 하기
+const navEl=document.querySelector('nav')
+const navMenuToggleEl=navEl.querySelector('nav .menu-toggler')
+const navMenuShadowEl=navEl.querySelector('.shadow')
+
+navMenuToggleEl.addEventListener('click',function(){
+  if(navEl.classList.contains('menuing')){
+    hideNavMenu()
+  }else{
+    showNavMenu()
+  }
+})
+navEl.addEventListener('click',function(event){
+  event.stopPropagation()
+})
+navMenuShadowEl.addEventListener('click',hideNavMenu)
+window.addEventListener('click',hideNavMenu)
+
+function showNavMenu(){
+  navEl.classList.add('menuing')
+}
+
+function hideNavMenu(){
+  navEl.classList.remove('menuing')
+}
+
 
 // 요소의 가시성 관찰
 const io = new IntersectionObserver(function (entries) {
@@ -158,6 +228,7 @@ navigations.forEach(function (nav) {
   mapEl.innerHTML =/*html*/`
   <h3>
     <span class="text">${nav.title}</span>
+    <span class='icon'>+</span>
   </h3>
   <ul>
   ${mapList}
@@ -169,3 +240,12 @@ navigations.forEach(function (nav) {
 
 const thisYearEl=document.querySelector('span.this-year')
 thisYearEl.textContent=new Date().getFullYear()
+
+const mapEls=document.querySelectorAll('footer .navigations .map')
+mapEls.forEach(function(el){
+  const h3El=el.querySelector('h3')
+  h3El.addEventListener('click',function(){
+    el.classList.toggle('active')
+    // toggle은 add.remove를 동시에 수행한다
+  })
+})
